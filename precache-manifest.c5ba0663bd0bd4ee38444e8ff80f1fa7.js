0b6837209c5f96aa01afc5570d646fa274a1ce81
Egor Mikheev <egor.mikheev@humaniq.com>
2021-12-13 14:53:33 +0300

    Updates

---
+++ precache-manifest.c5ba0663bd0bd4ee38444e8ff80f1fa7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce7c86a7d4996793a7676a3735e0caea",
+    "url": "/humaniq-dapps-home/index.html"
+  },
+  {
+    "revision": "e271d5f345c5bfa6c7bb",
+    "url": "/humaniq-dapps-home/static/css/main.09fa9a86.chunk.css"
+  },
+  {
+    "revision": "855231398015aa70e0d8",
+    "url": "/humaniq-dapps-home/static/js/2.031ca53c.chunk.js"
+  },
+  {
+    "revision": "e271d5f345c5bfa6c7bb",
+    "url": "/humaniq-dapps-home/static/js/main.0299035a.chunk.js"
+  },
+  {
+    "revision": "7c681f36a052da7cefc4",
+    "url": "/humaniq-dapps-home/static/js/runtime~main.747176e3.js"
+  },
+  {
+    "revision": "6172a9b9cb7a452e162f503d8f5cc5ed",
+    "url": "/humaniq-dapps-home/static/media/back-icon.6172a9b9.svg"
+  },
+  {
+    "revision": "59df06f15224d8615bfcf21715104886",
+    "url": "/humaniq-dapps-home/static/media/close-icon.59df06f1.svg"
+  },
+  {
+    "revision": "2145b025be0d35c799efd21c989f9d96",
+    "url": "/humaniq-dapps-home/static/media/logo-wordmark.2145b025.svg"
+  },
+  {
+    "revision": "7bef3eb64ba0aa8c1a53abbebba8dbd8",
+    "url": "/humaniq-dapps-home/static/media/logo.7bef3eb6.svg"
+  }
+]);
